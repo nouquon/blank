ee0ad0d0192a8c42531e21cc73bd98b15eae20a7
nouquon <nouquon@gmail.com>
2026-05-07 06:18:46 +0000

    update alpinefs

--- alpine.sh
+++ alpine.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 dir=/workspaces/blank/alpinefs
 if [ ! -d "$dir/proc/fs" ]; then
-  for fs in proc sys dev ; do
+  for fs in proc sys dev tmp ; do
     sudo mount --rbind /$fs $dir/$fs
   done
 fi
 #echo "nameserver 1.1.1.1" > $dir/etc/resolv.conf
-sudo chroot $dir /bin/su -l
+sudo chroot $dir /bin/ash
